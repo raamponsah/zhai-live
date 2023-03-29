3d8b78880c9b7c77aee899dc7145c76734aa38cd
Ralph <ramponsah026@st.ug.edu.gh>
2023-03-29 10:46:29 +0000

    navigation styles updated

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,47 +1,91 @@
-import Link from 'next/link'
-import React from 'react'
-import styles from '@/styles/Navigation.module.css'
+import Link from "next/link";
+import styles from "@/styles/Navigation.module.css";
 const navItems = [
-    {
-        name: 'Home',
-        link: '/'
-    },
-    {
-        name: 'About',
-        link: '/about'
-    },
-    {
-        name: 'Contact',
-        link: '/contact'
-    },
-    {
-        name: 'Donate',
-        link: '/donate'
-    }
-]
-
+  {
+    name: "Initiatives",
+    link: "/",
+  },
+  {
+    name: "Founder",
+    link: "/founder",
+  },
+  {
+    name: "About us",
+    link: "/about",
+  },
+  {
+    name: "News",
+    link: "/news",
+  },
+  {
+    name: "Contact",
+    link: "/contact",
+  },
+];
 
 const Navigation = () => {
   return (
     <>
-  
-        <nav className={styles.navigation}>
+      <nav className={styles.navigation}>
         <h1>Zhai Foundation</h1>
-            <ul>
-                {navItems.map((item, index) => (
-                    <li key={index}>
-                        <Link href={item.link}>{item.name}</Link>
-                    </li>
-                ))}
-            </ul>
+        <ul>
+          {navItems.map((item, index) => (
+            <li key={index}>
+              <Link href={item.link}>{item.name}</Link>
+            </li>
+          ))}
+        </ul>
 
-            <Link href=''>
-    <span>Donate</span>
-</Link>
-        </nav>
+        <div className={styles.hamburger} onClick={()=>{console.log('clicked')}}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 14 14"
+            height="24"
+            width="24"
+          >
+            <g>
+              <line
+                x1="13.5"
+                y1="2"
+                x2="0.5"
+                y2="2"
+                fill="none"
+                stroke="#000000"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              ></line>
+              <line
+                x1="13.5"
+                y1="7"
+                x2="0.5"
+                y2="7"
+                fill="none"
+                stroke="#000000"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              ></line>
+              <line
+                x1="13.5"
+                y1="12"
+                x2="0.5"
+                y2="12"
+                fill="none"
+                stroke="#000000"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              ></line>
+            </g>
+          </svg>
+        </div>
 
+        <Link href="" className={styles.ctaButton}>
+          <span>Donate</span>
+        </Link>
+
+      
+      </nav>
     </>
-  )
-}
+  );
+};
 
-export default Navigation
+export default Navigation;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import {Josefin_Sans} from '@next/font/google'
+
+const font = Josefin_Sans({
+  subsets:['latin'],
+  weight:['100','200','300','400','500','600','700'],
+})
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (<main className={font.className}>
+    <Component {...pageProps} />
+    </main>)
 }
