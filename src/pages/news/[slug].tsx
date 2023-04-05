1dd7f6b685878dd2df9ac6808c36ad1978522f5b
Ralph <ramponsah026@st.ug.edu.gh>
2023-04-05 20:01:44 +0000

    update paths

--- src/pages/news/[slug].tsx
+++ src/pages/news/[slug].tsx
@@ -12,6 +12,7 @@ type DataObjectType = {
 export const getStaticProps = async () => {
   const res = await fetch("http://localhost:1337/api/articles?populate=*");
   const data = await res.json();
+  console.log()
   return {
     props: { data },
   };
@@ -19,9 +20,10 @@ export const getStaticProps = async () => {
 
 export const getStaticPaths = async () => {
   const res = await fetch("https://zhai-strapi-cms-production.up.railway.app/api/articles");
-  const data:DataObjectType[] = await res.json();
+  const {data} = await res.json();
+  
   const paths = data.map((d:DataObjectType)=>{
-      return {params: {slug:d?.id}}
+      return {params: {slug:d?.attributes.Title.split(' ').join('-').toLowerCase()}}
   })
   return { paths, fallback: false };
 };
