40dacadf260120ba260936de020d012ccea5d013
Ralph <ramponsah026@st.ug.edu.gh>
2023-04-05 20:13:17 +0000

    title

--- src/pages/news/[slug].tsx
+++ src/pages/news/[slug].tsx
@@ -1,16 +1,18 @@
 import React from "react";
 
 type DataObjectType = {
-    id: number;
-    attributes: {
-      Title: string;
-      Content: string;
-      Cover:  { data: {attributes:{url:string}}  };
-    }
-  }
+  id: number;
+  attributes: {
+    Title: string;
+    Content: string;
+    Cover: { data: { attributes: { url: string } } };
+  };
+};
 
 export const getStaticProps = async () => {
-  const res = await fetch("https://zhai-strapi-cms-production.up.railway.app/api/articles?populate=*");
+  const res = await fetch(
+    "https://zhai-strapi-cms-production.up.railway.app/api/articles?populate=*"
+  );
   const data = await res.json();
   return {
     props: { data },
@@ -18,17 +20,23 @@ export const getStaticProps = async () => {
 };
 
 export const getStaticPaths = async () => {
-  const res = await fetch("https://zhai-strapi-cms-production.up.railway.app/api/articles");
-  const {data} = await res.json();
-  
-  const paths = data.map((d:DataObjectType)=>{
-      return {params: {slug:d?.attributes['Title'].split(' ').join('-').toLowerCase()}}
-  })
+  const res = await fetch(
+    "https://zhai-strapi-cms-production.up.railway.app/api/articles"
+  );
+  const { data } = await res.json();
+
+  const paths = data.map((d: DataObjectType) => {
+    return {
+      params: {
+        slug: d?.attributes["Title"].split(" ").join("-").toLowerCase(),
+      },
+    };
+  });
   return { paths, fallback: false };
 };
 
-const NewsArticle = ({ data }: {data: DataObjectType}) => {
-  return <div>NewsArticle {data?.attributes['Title']}</div>;
+const NewsArticle = ({ data }: { data: DataObjectType }) => {
+  return <div>NewsArticle {data?.attributes["Title"]}</div>;
 };
 
 export default NewsArticle;
