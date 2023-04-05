4bc9dff5cfdad8d82ed17209bf21ad5ade8a7b18
Ralph <ramponsah026@st.ug.edu.gh>
2023-04-05 19:42:44 +0000

    update gsp

--- src/pages/news/[slug].tsx
+++ src/pages/news/[slug].tsx
@@ -19,7 +19,7 @@ export const getStaticProps = async () => {
 
 export const getStaticPaths = async () => {
   const res = await fetch("https://zhai-strapi-cms-production.up.railway.app/api/articles");
-  const data = await res.json();
+  const data:DataObjectType[] = await res.json();
   const paths = data.map((d:DataObjectType)=>{
       return {params: {slug:d?.id}}
   })
