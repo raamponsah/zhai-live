00ac9fb31be938e00705592aba8f2955b7452849
Ralph <ramponsah026@st.ug.edu.gh>
2023-04-06 09:01:45 +0000

    dangerous

--- src/components/NewsItemComponent.tsx
+++ src/components/NewsItemComponent.tsx
@@ -26,8 +26,8 @@ const NewsItemComponent = ({ id, attributes }: DataObjectType) => {
 
       <h3>{attributes["Title"]}</h3>
       <h5>3rd May 2023 | Jeremy Phelps</h5>
-      <div>
-      { DOMPurify.sanitize(marked.parse(`${attributes['Excerpt']}`), {USE_PROFILES: {html: true}})}
+      <div dangerouslySetInnerHTML={{__html:DOMPurify.sanitize(marked.parse(`${attributes['Excerpt']}`), {USE_PROFILES: {html: true}})}}>
+
       </div>
 
       <div className={styles.controls}>
