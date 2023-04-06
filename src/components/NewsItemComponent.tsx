c3235faaef867d0c0899be28c17d194e690d4228
Ralph <ramponsah026@st.ug.edu.gh>
2023-04-06 10:11:31 +0000

    normal 2

--- src/components/NewsItemComponent.tsx
+++ src/components/NewsItemComponent.tsx
@@ -2,11 +2,10 @@ import Image from "next/image";
 import styles from "@/styles/NewsItemComponent.module.css";
 import Link from "next/link";
 import { DataObjectType } from "@/pages/news/[slug]";
-import { marked } from 'marked';
-import { JSDOM } from 'jsdom';
+import { marked } from "marked";
+import { JSDOM } from "jsdom";
 // import DOMPurify from 'dompurify';
-import DOMPurify from 'isomorphic-dompurify';
-
+import DOMPurify from "isomorphic-dompurify";
 
 const NewsItemComponent = ({ id, attributes }: DataObjectType) => {
   // const excerptFunction = (content:string)=>{
@@ -16,24 +15,33 @@ const NewsItemComponent = ({ id, attributes }: DataObjectType) => {
   return (
     <div className={styles.newsItem}>
       <div className={styles.imageContainer}>
-        <img src="https://res.cloudinary.com/dozaqkiam/image/upload/v1680734637/b206a7d6c5f91f26188bd38e52e6178f_d4330b6f1a.png
-" alt="" />
+        <img src={attributes["Cover"]?.data?.attributes?.url} alt="" />
       </div>
 
       <h3>{attributes["Title"]}</h3>
-      <h6>{attributes['Cover']?.data?.attributes?.url}</h6>
+      <h6>{attributes["Cover"]?.data?.attributes?.url}</h6>
       <h5>3rd May 2023 | Jeremy Phelps</h5>
-      <div dangerouslySetInnerHTML={{__html:DOMPurify.sanitize(marked.parse(`${attributes['Content']}`), {USE_PROFILES: {html: true}})}}>
-
-      </div>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: DOMPurify.sanitize(marked.parse(`${attributes["Content"]}`), {
+            USE_PROFILES: { html: true },
+          }),
+        }}
+      ></div>
 
       <div className={styles.controls}>
         <h6>3rd May 2023</h6>
         <h6>
-          <Link href={`/news/${attributes['Title'].split(' ').join('-').toLocaleLowerCase()}`}>Read on &rarr;</Link>
+          <Link
+            href={`/news/${attributes["Title"]
+              .split(" ")
+              .join("-")
+              .toLocaleLowerCase()}`}
+          >
+            Read on &rarr;
+          </Link>
         </h6>
-        </div>
-        
+      </div>
     </div>
   );
 };
