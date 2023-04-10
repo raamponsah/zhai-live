c865f1637dfc2207f6fdc698ccebb903be92c184
Ralph <ramponsah026@st.ug.edu.gh>
2023-04-10 22:35:34 +0000

    img

--- src/components/NewsItemComponent.tsx
+++ src/components/NewsItemComponent.tsx
@@ -6,24 +6,24 @@ import { marked } from "marked";
 // import DOMPurify from 'dompurify';
 import DOMPurify from "isomorphic-dompurify";
 import { CldImage } from "next-cloudinary";
-import { tsToUtcFormat, tsToLocalFormat } from 'date-format-ms';
-
-
+import { tsToUtcFormat, tsToLocalFormat } from "date-format-ms";
 
 const NewsItemComponent = (article: DataObjectType) => {
   return (
     <div className={styles.newsItem}>
       <div className={styles.imageContainer}>
-
-        <Image
-          width="600"
-          height="600"
+        <img
           src={article.attributes["Cover"]?.data?.attributes?.url}
           alt={article.attributes["Title"]}
         />
 
         <h3>{article.attributes["Title"]}</h3>
-        <h5 style={{fontStyle:'italic'}}>{tsToUtcFormat(Date.parse(`${article.attributes.publishedAt}`), 'D d M Y')}</h5>
+        <h5 style={{ fontStyle: "italic" }}>
+          {tsToUtcFormat(
+            Date.parse(`${article.attributes.publishedAt}`),
+            "D d M Y"
+          )}
+        </h5>
         {/* <h5>3rd May 2023 | Jeremy Phelps</h5> */}
 
         <div
@@ -38,7 +38,12 @@ const NewsItemComponent = (article: DataObjectType) => {
         ></div>
 
         <div className={styles.controls}>
-      <h6>{tsToUtcFormat(Date.parse(`${article.attributes.publishedAt}`), 'D d M Y')}</h6>
+          <h6>
+            {tsToUtcFormat(
+              Date.parse(`${article.attributes.publishedAt}`),
+              "D d M Y"
+            )}
+          </h6>
 
           <h6>
             <Link
