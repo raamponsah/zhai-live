9615c98717a570fbbe3c3fd943735f7f3bb2eca9
Ralph <ramponsah026@st.ug.edu.gh>
2023-04-14 21:10:56 +0000

    updates

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -12,6 +12,7 @@ const ProjectsPage = ({data}:{data:any}) => {
       <Navigation></Navigation>
       <FullWidthSection
         color="white"
+       
         title="Projects"
         subphrase="The Zhai Foundation focuses on several causes aimed at improving the lives of disadvantaged and marginalized communities in Africa. These include education, health, widowhood support, and orphanage support. The foundation provides educational opportunities, healthcare services, and support to widows and orphans to help them achieve a better quality of life. The Zhai Foundation is committed to empowering individuals and communities to overcome challenges and reach their full potential."
       ></FullWidthSection>
