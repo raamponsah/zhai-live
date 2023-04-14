98d9dcc032072774c49af8c5dd0da75cb31ceaa3
Ralph <ramponsah026@st.ug.edu.gh>
2023-04-14 11:28:58 +0000

    founder page updated

--- src/pages/founder.tsx
+++ src/pages/founder.tsx
@@ -14,6 +14,7 @@ const FounderPage = () => {
         subtitle="Founder, ZHAI"
         subphrase="Zhuang Zihao Brandon is the founder of the Zhai Foundation, a non-profit organization dedicated to improving the lives of disadvantaged and marginalized communities in Africa. He is a young Chinese man with a passion for supporting African youth and empowering them to reach their full potential through education, healthcare, and other forms of support. Through his work with the Zhai Foundation, Brandon has made a significant impact in the lives of many individuals and communities in Africa. His dedication to social impact and philanthropy has earned him recognition as a rising leader in the non-profit sector."
         bgColor="rgb(229 185 236 / 40%)"
+        flipped={true}
       >
          <Image
           style={{ borderRadius: "5px" }}
