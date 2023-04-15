257a820a315d84357053cb73869f4e277e1f1d0e
Ralph <ramponsah026@st.ug.edu.gh>
2023-04-15 10:47:41 +0000

    about page changes

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -16,8 +16,8 @@ const About = () => {
         bgColor="purple"
         color="white"
         title="ZHAI Foundation"
-        subtitle="We are a unique charity foundation that is dedicated to helping the world’s most vulnerable people. We are committed to empowering underserved communities and promoting sustainable development. We are a force to be reckoned with."
-        subphrase="Zhai Foundation is a beacon of hope and progress in the world of philanthropy. With a steadfast commitment to empowering underserved communities and promoting sustainable development, Zhai Foundation is a force to be reckoned with. Through innovative programs and strategic partnerships, they are making a tangible difference in the lives of people around the globe. Their unwavering dedication to social justice and environmental stewardship is an inspiration to us all. Zhai Foundation is truly a game-changer, and their impact will be felt for generations to come."
+        subtitle="Empowering Africa's Future"
+        subphrase="The Foundation is a beacon of hope and progress in the world of philanthropy. With a steadfast commitment to empowering underserved communities and promoting sustainable development, Zhai Foundation is a force to be reckoned with. Through innovative programs and strategic partnerships, they are making a tangible difference in the lives of people around the globe. Their unwavering dedication to social justice and environmental stewardship is an inspiration to us all. Zhai Foundation is truly a game-changer, and their impact will be felt for generations to come."
       ></FullWidthSection>
 
       <FullWidthSection
