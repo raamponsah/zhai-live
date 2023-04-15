6779e5011ed75c7e9de5a83873e3f6d2922193ea
Ralph <ramponsah026@st.ug.edu.gh>
2023-04-15 11:18:34 +0000

    meta tags added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,34 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
-import {Josefin_Sans} from '@next/font/google'
-import Head from 'next/head'
+import { Josefin_Sans } from "@next/font/google";
+import Head from "next/head";
 
 const font = Josefin_Sans({
-  subsets:['latin'],
-  weight:['100','200','300','400','500','600','700'],
-})
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700"],
+});
 export default function App({ Component, pageProps }: AppProps) {
-  return (<>
-    <Head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-   </Head>
-  <main className={font.className}>
-    <Component {...pageProps} />
-    </main></>)
+  return (
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+        <meta
+          name="description"
+          content="Zhai Foundation is a non-profit organization committed to improving the lives of disadvantaged and marginalized communities in Africa. Our causes include education, health, widowhood support, and orphanage support."
+        />
+        <meta
+          name="keywords"
+          content="Zhai Foundation, non-profit, Africa, education, health, widowhood support, orphanage support"
+        ></meta>
+          <meta name="author" content="Zhai Foundation"/>
+        <title>Zhai Foundation | Empowering Communities in Africa</title>
+      </Head>
+      <main className={font.className}>
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
 }
